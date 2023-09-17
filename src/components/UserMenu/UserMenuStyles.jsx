6d54f9b178dcaf2461375ab9be3ecabd5eefc7a9
Metaldetect <stas.klymenko@icloud.com>
2023-09-17 11:38:03 +0200

    Update UserMenuStyles.jsx

--- src/components/UserMenu/UserMenuStyles.jsx
+++ src/components/UserMenu/UserMenuStyles.jsx
@@ -3,3 +3,7 @@ import styled from '@emotion/styled';
 export const Container = styled.div``;
 export const Text = styled.p``;
 export const Button = styled.button``;
+export const Avatar = styled.img`
+  margin-right: 4px;
+  width: 40px;
+`;
