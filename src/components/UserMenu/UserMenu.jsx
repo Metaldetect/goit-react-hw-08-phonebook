8f83aff6b1ab3b3ce7630b17e878a4fdcc1a5e6b
Metaldetect <stas.klymenko@icloud.com>
2023-09-17 11:35:55 +0200

    update
    add avatar

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,14 +1,17 @@
 import { useDispatch } from 'react-redux';
 import { logOut } from 'redux/auth/operation';
 import useAuth from 'hooks/useAuth';
-import { Container, Button, Text } from './UserMenuStyles';
+import defaultAvatar from './default-avatar.png';
+import { Container, Button, Text, Avatar } from './UserMenuStyles';
 
 const UserMenu = () => {
   const dispatch = useDispatch();
   const { user } = useAuth();
+  const avatar = defaultAvatar;
 
   return (
     <Container>
+      <Avatar src={avatar} alt="avatar" />
       <Text>Welcome, {user.name}</Text>
       <Button type="button" onClick={() => dispatch(logOut())}>
         Logout
