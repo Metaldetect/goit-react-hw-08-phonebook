6b1520d13ae0f42afd020255cad87125346a7212
Metaldetect <stas.klymenko@icloud.com>
2023-09-18 16:06:30 +0200

    Update RegisterFormStyles.jsx

--- src/components/RegisterForm/RegisterFormStyles.jsx
+++ src/components/RegisterForm/RegisterFormStyles.jsx
@@ -1,13 +1,13 @@
 import styled from '@emotion/styled';
 
-export const FormContainer = styled.form`
-  max-width: 300px;
-  margin: 0 auto;
-  padding: 20px;
-  border: 1px solid #ccc;
-  border-radius: 5px;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-`;
+// export const Form = styled.form`
+//   max-width: 300px;
+//   margin: 0 auto;
+//   padding: 20px;
+//   border: 1px solid #ccc;
+//   border-radius: 5px;
+//   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+// `;
 
 export const FormGroup = styled.div`
   margin-bottom: 15px;
