1a4b05fa70db25115f8cbd8d5203539052d4d594
Metaldetect <stas.klymenko@icloud.com>
2023-09-21 06:55:24 +0200

    Update selectors.js

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,6 +1,6 @@
 export const selectIsLoading = state => state.contacts.isLoading;
 
-export const selectContacts = state => state.contacts;
+export const selectContacts = state => state.contacts.items;
 
 export const selectError = state => state.contacts.error;
 
