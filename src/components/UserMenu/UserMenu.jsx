024f03908f81b632b49b48c42a8a4ce8788671a1
Metaldetect <stas.klymenko@icloud.com>
2023-09-24 16:10:59 +0200

    Update UserMenu.jsx

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -2,19 +2,18 @@ import React from 'react';
 import { useDispatch } from 'react-redux';
 import { logOut } from 'redux/auth/operation';
 import useAuth from 'hooks/useAuth';
-import defaultAvatar from './default-avatar.png';
+
 import { Avatar, Box, Button, Typography, Container } from '@mui/material';
 
 const UserMenu = () => {
   const dispatch = useDispatch();
   const { user } = useAuth();
-  const avatar = defaultAvatar;
 
   return (
     <Container>
       <Box display="flex" alignItems="center" p={2}>
         <Avatar
-          src={avatar}
+          src="https://source.unsplash.com/random"
           alt="avatar"
           sx={{ width: 50, height: 50, marginRight: 2 }}
         />
