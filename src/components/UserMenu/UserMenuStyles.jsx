e6481a7f6b5669a6ecfcdd5255cfc29fb448d31a
Metaldetect <stas.klymenko@icloud.com>
2023-09-24 20:48:11 +0200

    Delete UserMenuStyles.jsx

--- src/components/UserMenu/UserMenuStyles.jsx
+++
@@ -1,9 +0,0 @@
-import styled from '@emotion/styled';
-
-export const Container = styled.div``;
-export const Text = styled.p``;
-export const Button = styled.button``;
-export const Avatar = styled.img`
-  margin-right: 4px;
-  width: 40px;
-`;
