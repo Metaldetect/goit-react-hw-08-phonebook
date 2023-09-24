0fcda2818bb562147d7a42c2e2024aef80c9a6f5
Metaldetect <stas.klymenko@icloud.com>
2023-09-24 20:09:24 +0200

    Update Navigation.jsx

--- src/components/Navigation/Navigation.jsx
+++
@@ -1,9 +0,0 @@
-// import { NavLink } from 'react-router-dom';
-// import useAuth from 'hooks/useAuth';
-
-const Navigation = () => {
-  // const { isLoggedIn } = useAuth();
-  // return <>{isLoggedIn && <NavLink to="/contacts">Contacts</NavLink>}</>;
-};
-
-export default Navigation;
