71fd835eddc6ab185c1aa91e062da9323df5dc6d
Metaldetect <stas.klymenko@icloud.com>
2023-09-24 13:51:19 +0200

    Update Filter.jsx

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
-import { FilterWrapper, FilterLabel, FilterInput } from './FilterStyles';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectContactsFilter } from 'redux/selectors';
 import { setFilter } from 'redux/filterSlice';
+import TextField from '@mui/material/TextField';
+import InputLabel from '@mui/material/InputLabel';
+import FormControl from '@mui/material/FormControl';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
 
 function FilterComponent() {
   const dispatch = useDispatch();
@@ -11,18 +15,34 @@ function FilterComponent() {
   const handleFilterChange = e => {
     dispatch(setFilter(e.target.value.trim()));
   };
+
   return (
-    <FilterWrapper>
-      <FilterLabel htmlFor="filter">Filter contacts:</FilterLabel>
-      <FilterInput
-        type="text"
-        placeholder="Enter a name"
-        id="filter"
-        name="filter"
-        value={filter}
-        onChange={handleFilterChange}
-      />
-    </FilterWrapper>
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        mt: 3,
+        mb: 5,
+      }}
+    >
+      <FormControl>
+        <Typography variant="h5" align="center" gutterBottom>
+          Filter contacts:
+        </Typography>
+        <InputLabel htmlFor="filter"></InputLabel>
+        <TextField
+          type="text"
+          placeholder="Enter a name"
+          id="filter"
+          name="filter"
+          value={filter}
+          onChange={handleFilterChange}
+          sx={{ width: '100%', marginTop: '16px', mt: 1 }}
+        />
+      </FormControl>
+    </Box>
   );
 }
 
