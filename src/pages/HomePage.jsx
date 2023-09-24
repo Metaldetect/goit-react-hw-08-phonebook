614f8b6691fcbbf48df7501c2c890ca2d03f66ed
Metaldetect <stas.klymenko@icloud.com>
2023-09-24 20:54:58 +0200

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,24 +1,9 @@
 import React from 'react';
 import LoginForm from 'components/LoginForm';
-import { Container, Typography } from '@mui/material';
+import { Container } from '@mui/material';
 
 const Home = () => (
-  <Container
-    sx={{
-      minHeight: 'calc(50vh - 50px)',
-      minWidth: 'calc(50vw - 50px)',
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-      flexDirection: 'column',
-    }}
-  >
-    <Typography
-      variant="h1"
-      sx={{ fontWeight: 700, fontSize: 30, textAlign: 'center' }}
-    >
-      Your Page Title
-    </Typography>
+  <Container>
     <LoginForm />
   </Container>
 );
