e0cfb11163b16003817dde94dfd525d88565065b
Metaldetect <stas.klymenko@icloud.com>
2023-09-24 20:47:22 +0200

    Update AppBar.jsx

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -2,28 +2,26 @@ import * as React from 'react';
 import { AppBar as MuiAppBar, Toolbar, Typography, Box } from '@mui/material';
 import UserMenu from 'components/UserMenu';
 import useAuth from 'hooks/useAuth';
-import { Header } from './AppBarStyles';
+
 import { Link, NavLink } from 'react-router-dom';
 
 function AppBar() {
   const { isLoggedIn } = useAuth();
 
   return (
-    <Header>
-      <MuiAppBar position="static">
-        <Toolbar>
-          {!isLoggedIn && (
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              <Link to="/">Phonebook</Link>
-            </Typography>
-          )}
-          <Typography variant="h6">
-            {isLoggedIn && <NavLink to="/contacts">Contacts</NavLink>}
+    <MuiAppBar position="static">
+      <Toolbar>
+        {!isLoggedIn && (
+          <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
+            <Link to="/">Phonebook</Link>
           </Typography>
-          <Box sx={{ marginLeft: 'auto' }}>{isLoggedIn && <UserMenu />}</Box>
-        </Toolbar>
-      </MuiAppBar>
-    </Header>
+        )}
+        <Typography variant="h5">
+          {isLoggedIn && <NavLink to="/contacts">Contacts</NavLink>}
+        </Typography>
+        <Box sx={{ marginLeft: 'auto' }}>{isLoggedIn && <UserMenu />}</Box>
+      </Toolbar>
+    </MuiAppBar>
   );
 }
 
