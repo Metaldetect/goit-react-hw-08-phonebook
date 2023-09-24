52bc467c437c53c190a4d10054515b47fe3f3aec
Metaldetect <stas.klymenko@icloud.com>
2023-09-24 13:26:40 +0200

    Update UserMenu.jsx

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,8 +1,9 @@
+import React from 'react';
 import { useDispatch } from 'react-redux';
 import { logOut } from 'redux/auth/operation';
 import useAuth from 'hooks/useAuth';
 import defaultAvatar from './default-avatar.png';
-import { Container, Button, Text, Avatar } from './UserMenuStyles';
+import { Avatar, Box, Button, Typography, Container } from '@mui/material';
 
 const UserMenu = () => {
   const dispatch = useDispatch();
@@ -11,11 +12,23 @@ const UserMenu = () => {
 
   return (
     <Container>
-      <Avatar src={avatar} alt="avatar" />
-      <Text>Welcome, {user.name}</Text>
-      <Button type="button" onClick={() => dispatch(logOut())}>
-        Logout
-      </Button>
+      <Box display="flex" alignItems="center" p={2}>
+        <Avatar
+          src={avatar}
+          alt="avatar"
+          sx={{ width: 50, height: 50, marginRight: 2 }}
+        />
+        <Typography variant="h6" gutterBottom sx={{ marginRight: 2 }}>
+          Welcome, {user.name}
+        </Typography>
+        <Button
+          variant="contained"
+          sx={{ backgroundColor: 'grey', color: 'white' }}
+          onClick={() => dispatch(logOut())}
+        >
+          Logout
+        </Button>
+      </Box>
     </Container>
   );
 };
