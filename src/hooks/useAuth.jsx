710e7e9c7088b238164bc3aa885809ce9d4f96b6
Metaldetect <stas.klymenko@icloud.com>
2023-09-24 13:26:43 +0200

    Update useAuth.jsx

--- src/hooks/useAuth.jsx
+++ src/hooks/useAuth.jsx
@@ -3,18 +3,21 @@ import {
   selectEmail,
   selectIsLoggedIn,
   selectIsRefreshing,
+  selectIsAuthenticated,
   selectUser,
 } from 'redux/auth/selectors';
 
 const useAuth = () => {
   const isLoggedIn = useSelector(selectIsLoggedIn);
   const isRefreshing = useSelector(selectIsRefreshing);
+  const isAuthenticated = useSelector(selectIsAuthenticated);
   const user = useSelector(selectUser);
   const email = useSelector(selectEmail);
 
   return {
     isLoggedIn,
     isRefreshing,
+    isAuthenticated,
     user,
     email,
   };
