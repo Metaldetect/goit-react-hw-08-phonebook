e4806a4eebba181cf85dde67bd528757b05cb504
Metaldetect <stas.klymenko@icloud.com>
2023-09-24 20:48:13 +0200

    Update ContactsPage.jsx

--- src/pages/ContactsPage.jsx
+++ src/pages/ContactsPage.jsx
@@ -1,5 +1,4 @@
 import { Helmet, HelmetProvider } from 'react-helmet-async';
-
 import ContactForm from 'components/ContactForm';
 import ContactList from 'components/ContactList';
 import Filter from 'components/Filter';
