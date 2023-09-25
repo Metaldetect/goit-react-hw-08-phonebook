39fc9797362762b658f874119e506f320fa508e9
Metaldetect <stas.klymenko@icloud.com>
2023-09-25 04:32:31 +0200

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
-import LoginForm from 'components/LoginForm';
-import { Container } from '@mui/material';
+import { Container, Typography } from '@mui/material';
 
-const Home = () => (
-  <Container>
-    <LoginForm />
-  </Container>
-);
+const Home = () => {
+  return (
+    <Container>
+      <Typography
+        variant="h4"
+        sx={{ fontWeight: 700, fontSize: 30, textAlign: 'center' }}
+      >
+        Welcome to the Home Page!
+      </Typography>
+    </Container>
+  );
+};
 
 export default Home;
