24a8bc60b06aaf8177a6aedc3e63ce0a232aa76c
Metaldetect <stas.klymenko@icloud.com>
2023-09-25 05:02:25 +0200

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,16 +1,30 @@
 import React from 'react';
 import { Container, Typography } from '@mui/material';
+import { useSpring, animated } from 'react-spring';
 
 const Home = () => {
+  const springProps = useSpring({
+    from: { opacity: 0, transform: 'translateY(-500px)' },
+    to: { opacity: 1, transform: 'translateY(0)' },
+    config: { duration: 2000 },
+  });
+
   return (
-    <Container>
-      <Typography
-        variant="h4"
-        sx={{ fontWeight: 700, fontSize: 30, textAlign: 'center' }}
-      >
-        Welcome to the Home Page!
-      </Typography>
-    </Container>
+    <animated.div style={springProps}>
+      <Container>
+        <Typography
+          variant="h4"
+          sx={{
+            marginTop: 40,
+            fontWeight: 700,
+            fontSize: 40,
+            textAlign: 'center',
+          }}
+        >
+          Welcome to the Home Page!
+        </Typography>
+      </Container>
+    </animated.div>
   );
 };
 
