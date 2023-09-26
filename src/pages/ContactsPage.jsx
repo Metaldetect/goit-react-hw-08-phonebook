2fcb3448e18a075394826fb858f783058dee4110
Metaldetect <stas.klymenko@icloud.com>
2023-09-26 20:20:17 +0200

    Update ContactsPage.jsx

--- src/pages/ContactsPage.jsx
+++ src/pages/ContactsPage.jsx
@@ -2,8 +2,17 @@ import { Helmet, HelmetProvider } from 'react-helmet-async';
 import ContactForm from 'components/ContactForm';
 import ContactList from 'components/ContactList';
 import Filter from 'components/Filter';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchContacts } from 'redux/constacts/contactsAsyncActions';
 
 function Contacts() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
   return (
     <>
       <HelmetProvider>
