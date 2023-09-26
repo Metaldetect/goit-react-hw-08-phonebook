55ca828a53919a9e62864ca448573d0d1265dbbc
Metaldetect <stas.klymenko@icloud.com>
2023-09-26 20:20:45 +0200

    Delete selectors.js

--- src/redux/selectors.js
+++
@@ -1,7 +0,0 @@
-export const selectIsLoading = state => state.contacts.isLoading;
-
-export const selectContacts = state => state.contacts.items;
-
-export const selectError = state => state.contacts.error;
-
-export const selectContactsFilter = state => state.filters;
